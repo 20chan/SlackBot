7221a6cda7ef69216cfb3a7f057c62b6dd1f51f7
philly <sonagi.philly@gmail.com>
2016-06-02 09:50:27 +0900

    update
    updated commands.

--- SlackBot/Bot.cs
+++ SlackBot/Bot.cs
@@ -11,16 +11,14 @@ namespace SlackBot
 	public class SlackBot
 	{
 		private Slack _slack;
-		private List<string> _admins;
-		private List<string> _banned;
+		private Data _data;
 		
 		public SlackBot(string server, string channel, string nick, string password)
 		{
 			this._slack = new Slack(server, channel, nick, password);
-			this._admins = new List<string>();
-			this._banned = new List<string>();
+			this._data = new Data();
 			
-			_slack.Connected += (b) => { _slack.SendMessages( _slack.Channel, new string[] { "> 코딩노예 봇 시작!", "> 사용법은 !도움말 을 입력해보세요!" }); };
+			_slack.Connected += (b) => { _slack.SendMessages( _slack.Channel, new string[] { "> 코딩노예 봇 시작!", "> 사용법은 !help 을 입력해보세요!" }); };
 			_slack.GotMessage += _slack_GotMessage;
 		}
 
@@ -42,19 +40,45 @@ namespace SlackBot
 		
 		public void Execute(string channel, string nick, string command, string message)
 		{
-			if(command == "help")
+			if(command == "help" || command == "도움말")
 			{
 				string[] help = new string[] {
 					"IRC SSL 연결을 사용하여 제작된 오픈소스 슬랙 봇입니다. 다양한 커맨드를 지원합니다.",
-					"깃헙 : https://github.com/PhillyAI/SlackBot"
+					"깃헙 : https://github.com/PhillyAI/SlackBot",
+					"명령어는 !으로 시작하여 입력합니다.",
+					"명령어 목록을를 보려면 !commands 를 입력해주세요."
 				};
 				_slack.SendMessages(channel, help);
 			}
-			else if(command == "hs")
+			else if(command == "command" || command == "cmd" || command == "명령어")
+			{
+				string[] commands = new string[] {
+					"!help !도움말 : 도움말을 보여줍니다.",
+					"!command !cmd !명령어 : 명령어 목록을 보여줍니다.",
+					"!hs {code} !haskell {code} !하스켈 {code} : 하스켈 코드를 실행시킨 후 결과를 리턴합니다."
+				};
+				_slack.SendMessages(channel, commands);
+			}
+			else if(command == "hs" || command == "haskell" || command == "하스켈")
 			{
 				string[] result = Interpreter.InterpreteHaskell(message);
 				_slack.SendMessages(channel, result);
 			}
+			else if(command == "날씨")
+			{
+				string[] result = Weather.GetWeather(message);
+				_slack.SendMessages(channel, result);
+			}
+		}
+		
+		public void Save(string path)
+		{
+			this._data.Save(path);
+		}
+		
+		public void Open(string path)
+		{
+			this._data = Data.FromFile(path);
 		}
 	}
 }
---
+++ SlackBot/Weather.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * 사용자: philly
+ * 날짜: 2016-06-01
+ * 시간: 오후 10:17
+ */
+using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SlackBot
+{
+	/// <summary>
+	/// 기상청 날씨 파싱 라이브러리
+	/// </summary>
+	public static class Weather
+	{
+		private static Dictionary<string, Point> regions = new Dictionary<string, Point>()
+		{
+			{"서울", new Point(59, 127)},
+			{"세종", new Point(65, 103)},
+			{"대전", new Point(67, 101)},
+			{"부산", new Point(93, 73)},
+			{"대구", new Point(88, 90)},
+			{"인천", new Point(51, 131)},
+			{"전남", new Point(51, 131)},
+			{"제주", new Point(56, 33)},
+			{"충남", new Point(65, 99)},
+			{"충북", new Point(76, 111)},
+			{"전북", new Point(55, 80)},
+			{"강원", new Point(93, 131)},
+			{"경기", new Point(69, 132)},
+			{"경남", new Point(89, 68)},
+			{"경북", new Point(91, 89)},
+			{"광주", new Point(57, 74)},
+			{"울산", new Point(102, 84)}
+		};
+		
+		public static string[] GetWeather(string region)
+		{
+			if(regions.ContainsKey(region))
+			{
+				Point p = regions[region];
+				return GetWeather(p.X, p.Y);
+			}
+			else
+			{
+				return new string[] { "지역이 테이블에 등록되지 않았습니다." };
+			}
+		}
+		
+		public static string[] GetWeather(int x, int y)
+		{
+			XmlDocument document = new XmlDocument();
+            try
+            {
+            	document.Load("http://www.kma.go.kr/wid/queryDFS.jsp?gridx=" + x.ToString() + "&gridy=" + y.ToString());
+            }
+            catch
+            {
+                return null;
+            }
+            XmlNodeList elementsByTagName = document.GetElementsByTagName("hour");
+            XmlNodeList list2 = document.GetElementsByTagName("temp");
+            XmlNodeList list3 = document.GetElementsByTagName("wfKor");
+            return new string[] {(elementsByTagName[0].InnerText + "시 : " + list3[0].InnerText + " (" + list2[0].InnerText + "℃)"), (elementsByTagName[1].InnerText + "시 : " + list3[1].InnerText + " (" + list2[1].InnerText + "℃)"), (elementsByTagName[2].InnerText + "시 : " + list3[2].InnerText + " (" + list2[2].InnerText + "℃)"), (elementsByTagName[3].InnerText + "시 : " + list3[3].InnerText + " (" + list2[3].InnerText + "℃)") };
+		}
+	}
+}
